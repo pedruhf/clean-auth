96d8fa1bb9858d5b11ec04f7437efc4163cd3e0e
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-11-27 19:10:58 -0300

    chore: adds vitest config

---
+++ clean-auth/vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config';
+import { resolve } from "path";
+
+export default defineConfig({
+  define: {
+    alias: [
+      { find: "@/tests", replacement: resolve(__dirname, "tests") },
+      { find: "@", replacement: resolve(__dirname, "src") }
+    ],
+  },
+  test: {
+    root: "<rootDir>/src",
+    environment: "node",
+    coverage: {
+      reportsDirectory: "coverage",
+    },
+    passWithNoTests: true,
+    globals: true,
+  }
+})

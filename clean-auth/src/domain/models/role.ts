b8a3810a928a085c4f576e24a4ea4f0cd33de90b
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-12-06 09:54:41 -0300

    feat: adds domain roles

---
+++ clean-auth/src/domain/models/role.ts
@@ -0,0 +1,31 @@
+export enum Roles {
+  admin = "admin",
+  developer = "developer",
+  user = "user",
+}
+
+export enum Permissions {
+  createAdmin = "create_admin",
+  createDeveloper = "create_developer",
+  listAll = "list_all",
+}
+
+export type Role = {
+  id: number;
+  name: Roles;
+  permissions: Permissions[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export const getAdminPermissions = () => [
+  Permissions.createAdmin,
+  Permissions.createDeveloper,
+  Permissions.listAll,
+];
+
+export const getDeveloperPermissions = () => [
+  Permissions.listAll,
+];
+
+export const getUserPermissions = () => [];
